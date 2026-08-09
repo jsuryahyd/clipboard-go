7238145c94772cb6efe15171d653ccf9cc9ffd90
jsuryahyd <jsuryahyd@gmail.com>
2026-08-09 07:20:44 +0530

    Update GNOME Extension to ESM module format for GNOME Shell 45/46

--- extension/extension.js
+++ extension/extension.js
@@ -1,6 +1,11 @@
-const { St, Clutter, GLib, Gio, Meta, Shell } = imports.gi;
-const Main = imports.ui.main;
-const ExtensionUtils = imports.misc.extensionUtils;
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import St from 'gi://St';
+import Clutter from 'gi://Clutter';
+import GLib from 'gi://GLib';
+import Gio from 'gi://Gio';
+import Meta from 'gi://Meta';
+import Shell from 'gi://Shell';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 const DBUS_IFACE = `
 <node>
@@ -17,36 +22,33 @@ const DBUS_IFACE = `
     </interface>
 </node>`;
 
-class ClipboardExtension {
-    constructor() {
+export default class ClipboardExtension extends Extension {
+    enable() {
+        console.log('[Clipboard-Go] Enabling extension bridge for GNOME 45+...');
         this._clipboard = St.Clipboard.get_default();
         this._lastContent = "";
         this._pollTimerId = 0;
         this._dbusImpl = null;
         this._keybindingName = 'toggle-clipboard-go';
-    }
-
-    enable() {
-        log('[Clipboard-Go] Enabling extension bridge...');
 
         // 1. Export DBus Interface on Session Bus
         try {
             this._dbusImpl = Gio.DBusExportedObject.wrapJSObject(DBUS_IFACE, this);
             this._dbusImpl.export(Gio.DBus.session, '/org/gnome/Shell/Extensions/ClipboardGo');
-            log('[Clipboard-Go] DBus interface exported at /org/gnome/Shell/Extensions/ClipboardGo');
+            console.log('[Clipboard-Go] DBus interface exported at /org/gnome/Shell/Extensions/ClipboardGo');
         } catch (e) {
-            log(`[Clipboard-Go] Error exporting DBus interface: ${e}`);
+            console.error(`[Clipboard-Go] Error exporting DBus interface: ${e}`);
         }
 
-        // 2. Start Clipboard Polling / Listener for Wayland compatibility
+        // 2. Start Clipboard Polling
         this._startClipboardMonitoring();
 
-        // 3. Register Global Hotkey (Super+Shift+V)
+        // 3. Register Global Hotkey
         this._registerHotkey();
     }
 
     disable() {
-        log('[Clipboard-Go] Disabling extension bridge...');
+        console.log('[Clipboard-Go] Disabling extension bridge...');
 
         // 1. Stop Clipboard Polling
         this._stopClipboardMonitoring();
@@ -62,7 +64,6 @@ class ClipboardExtension {
     }
 
     _startClipboardMonitoring() {
-        // Poll clipboard every 500ms (lightweight, non-blocking check)
         this._pollTimerId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, () => {
             this._checkClipboardContent();
             return GLib.SOURCE_CONTINUE;
@@ -82,7 +83,6 @@ class ClipboardExtension {
                 if (text && text.trim().length > 0 && text !== this._lastContent) {
                     this._lastContent = text;
                     if (this._dbusImpl) {
-                        log(`[Clipboard-Go] ClipboardChanged emitted (${text.length} chars)`);
                         this._dbusImpl.emit_signal('ClipboardChanged', new GLib.Variant('(ss)', ['text', text]));
                     }
                 }
@@ -94,21 +94,20 @@ class ClipboardExtension {
 
     _registerHotkey() {
         try {
-            const ModeType = Shell.ActionMode ? Shell.ActionMode : Shell.KeyBindingMode;
             Main.wm.addKeybinding(
                 this._keybindingName,
                 new Gio.Settings({ schema_id: 'org.gnome.shell.keybindings' }),
                 Meta.KeyBindingFlags.NONE,
-                ModeType.ALL,
+                Shell.ActionMode.ALL,
                 () => {
-                    log('[Clipboard-Go] Global hotkey pressed, emitting ShowUI signal');
+                    console.log('[Clipboard-Go] Global hotkey pressed, emitting ShowUI signal');
                     if (this._dbusImpl) {
                         this._dbusImpl.emit_signal('ShowUI', null);
                     }
                 }
             );
         } catch (e) {
-            log(`[Clipboard-Go] Keybinding registration warning: ${e}. Using fallback signal listener.`);
+            console.warn(`[Clipboard-Go] Keybinding warning: ${e}`);
         }
     }
 
@@ -122,30 +121,23 @@ class ClipboardExtension {
 
     // DBus Method: InjectPaste
     InjectPaste(type, content) {
-        log(`[Clipboard-Go] InjectPaste invoked (type=${type}, len=${content.length})`);
+        console.log(`[Clipboard-Go] InjectPaste invoked (type=${type}, len=${content.length})`);
         
-        // 1. Update GNOME Shell clipboard text
-        this._lastContent = content; // avoid re-recording our own pasted item
+        this._lastContent = content;
         this._clipboard.set_text(St.ClipboardType.CLIPBOARD, content);
 
-        // 2. Yield focus and simulate Ctrl+V keystroke into active window
         GLib.timeout_add(GLib.PRIORITY_DEFAULT, 50, () => {
             try {
                 const seat = Clutter.get_default_backend().get_default_seat();
                 if (seat) {
-                    const now = CLUTTER_CURRENT_TIME || 0;
-                    // Press Ctrl + V
+                    const now = 0;
                     seat.handle_event(Clutter.Event.new_key(Clutter.EventType.KEY_PRESS, now, Clutter.EventFlags.NONE, Clutter.ModifierType.CONTROL_MASK, Clutter.KEY_v, 0));
                     seat.handle_event(Clutter.Event.new_key(Clutter.EventType.KEY_RELEASE, now, Clutter.EventFlags.NONE, Clutter.ModifierType.CONTROL_MASK, Clutter.KEY_v, 0));
                 }
             } catch (err) {
-                log(`[Clipboard-Go] Clutter synthetic event error: ${err}`);
+                console.error(`[Clipboard-Go] Clutter synthetic event error: ${err}`);
             }
             return GLib.SOURCE_REMOVE;
         });
     }
-}
-
-function init() {
-    return new ClipboardExtension();
 }
